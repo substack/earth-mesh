7990f651b749f402f5ab02f393f79b21293a7d37
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-01-08 00:14:23 -1000

    refactor to handle more kinds of polygons

--- index.js
+++ index.js
@@ -9,56 +9,44 @@ module.exports = function (data, opts) {
   var mesh = { positions: [], cells: [] }
   var total = 0
   for (var i = 0; i < data.features.length; i++) {
-    var coords = data.features[i].geometry.coordinates
-    for (var j = 0; j < coords.length; j++) {
-      total += coords[j].length
+    var geo = data.features[i].geometry
+    var coords = geo.coordinates
+    if (geo.type === 'Polygon') {
+      for (var j = 0; j < coords.length; j++) {
+        total += coords[j].length
+      }
+    } else if (geo.type === 'MultiPolygon') {
+      for (var j = 0; j < coords.length; j++) {
+        for (var k = 0; k < coords[j].length; k++) {
+          total += coords[j][k].length
+        }
+      }
+    } else if (geo.type === 'LineString') {
+      total += coords.length
+    } else if (geo.type === 'Point') {
+      total++
     }
   }
   var progress = 0
   for (var i = 0; i < data.features.length; i++) {
-    var edges = [], points = []
-    var coords = data.features[i].geometry.coordinates
-    var xmin = Infinity, xmax = -Infinity
-    var ymin = Infinity, ymax = -Infinity
-    for (var j = 0; j < coords.length; j++) {
-      var n = points.length
-      var len = coords[j].length
-      var olen = len
-      for (var k = 0; k < len; k++) {
-        var p = coords[j][k]
-        var pp = coords[j][(k+1)%len]
-        var epsilon = 0.0001
-        if (aeq(p[0], pp[0], epsilon) && aeq(p[1], pp[1], epsilon)) {
-          // remove duplicates, hangs clean-pslg otherwise
-          coords[j].splice(k,1)
-          k--
-          len--
-        }
-      }
-      for (var k = 0; k < len; k++) {
-        var p = coords[j][k]
-        if (p[0] < xmin) xmin = p[0]
-        if (p[0] > xmax) xmax = p[0]
-        if (p[1] < ymin) ymin = p[1]
-        if (p[1] > ymax) ymax = p[1]
-        edges.push([n+k,n+(k+1)%len])
-        points.push(p)
-      }
-      progress += olen
+    var geo = data.features[i].geometry
+    var coords = geo.coordinates
+    if (geo.type === 'Polygon') {
+      add(polygon(coords))
+    } else if (geo.type === 'MultiPolygon') {
+      coords.forEach(function (c) {
+        add(polygon(c))
+      })
     }
-    clean(points, edges)
-    for (var y = Math.max(ymin,-82); y < Math.min(ymax,82); y+=4) {
-      for (var x = Math.max(xmin,-179.999); x < Math.min(xmax,179.999); x+=4) {
-        points.push([x,y]) // lon,lat
-      }
-    }
-    var cells = cdt(points, edges, { exterior: false, delaunay: true })
-    mesh.cells = mesh.cells.concat(cells.map(function (cs) {
+  }
+  function add (p) {
+    mesh.cells = mesh.cells.concat(p.cells.map(function (cs) {
       return cs.map(function (c) {
         return c + mesh.positions.length
       })
     }))
-    mesh.positions = mesh.positions.concat(points)
+    mesh.positions = mesh.positions.concat(p.positions)
+    progress += p.progress
     if (typeof opts.progress === 'function') {
       opts.progress(progress, total)
     }
@@ -67,7 +55,6 @@ module.exports = function (data, opts) {
     //
   } else if (opts.format === 'latlon') {
     for (var i = 0; i < mesh.positions.length; i++) {
-      console.error(mesh.positions[i])
       var lon = mesh.positions[i][0]
       var lat = mesh.positions[i][1]
       mesh.positions[i][0] = lat
@@ -93,4 +80,48 @@ module.exports = function (data, opts) {
     }
   }
   return mesh
+}
+
+function polygon (coords) {
+  var edges = [], points = []
+  var xmin = Infinity, xmax = -Infinity
+  var ymin = Infinity, ymax = -Infinity
+  var progress = 0
+  for (var j = 0; j < coords.length; j++) {
+    var n = points.length
+    var len = coords[j].length
+    var olen = len
+    for (var k = 0; k < len; k++) {
+      var p = coords[j][k]
+      var pp = coords[j][(k+1)%len]
+      var epsilon = 0.0001
+      if (aeq(p[0], pp[0], epsilon) && aeq(p[1], pp[1], epsilon)) {
+        // remove duplicates, hangs clean-pslg otherwise
+        coords[j].splice(k,1)
+        k--
+        len--
+      }
+    }
+    for (var k = 0; k < len; k++) {
+      var p = coords[j][k]
+      if (p[0] < xmin) xmin = p[0]
+      if (p[0] > xmax) xmax = p[0]
+      if (p[1] < ymin) ymin = p[1]
+      if (p[1] > ymax) ymax = p[1]
+      edges.push([n+k,n+(k+1)%len])
+      points.push(p)
+    }
+    progress += olen
+  }
+  clean(points, edges)
+  for (var y = Math.max(ymin,-82); y < Math.min(ymax,82); y+=4) {
+    for (var x = Math.max(xmin,-179.999); x < Math.min(xmax,179.999); x+=4) {
+      points.push([x,y]) // lon,lat
+    }
+  }
+  return {
+    progress: progress,
+    positions: points,
+    cells: cdt(points, edges, { exterior: false, delaunay: true })
+  }
 }
