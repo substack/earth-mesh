89c951a40824e9f23835dda74d14b64c552a5c2e
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-06-27 12:18:28 +0300

    polygons

--- index.js
+++ index.js
@@ -128,10 +128,10 @@ module.exports = function (mesh, f) {
               }
             }
           }
-          draw.linestrip.attributes.positions.push(g.coordinates[0])
+          draw.linestrip.attributes.positions.push(g.coordinates[j][0])
           draw.linestrip.count++
           if (f) {
-            var xattrs = f(m, g.coordinates[0], j, 0, 0)
+            var xattrs = f(m, g.coordinates[j][0], j, 0, 0)
             if (xattrs) {
               var keys = Object.keys(xattrs)
               for (var i = 0; i < keys.length; i++) {
@@ -143,11 +143,11 @@ module.exports = function (mesh, f) {
             }
           }
         }
-        for (var i = 0; i < g.coordinates.length; i++) {
-          draw.linestrip.attributes.positions.push(g.coordinates[i])
+        for (var i = 0; i < g.coordinates[j].length; i++) {
+          draw.linestrip.attributes.positions.push(g.coordinates[j][i])
           draw.linestrip.count++
           if (f) {
-            var xattrs = f(m, g.coordinates[i], j, i, 0)
+            var xattrs = f(m, g.coordinates[j][i], j, i, 0)
             if (xattrs) {
               var keys = Object.keys(xattrs)
               for (var i = 0; i < keys.length; i++) {
@@ -158,10 +158,10 @@ module.exports = function (mesh, f) {
               }
             }
           }
-          draw.linestrip.attributes.positions.push(g.coordinates[i])
+          draw.linestrip.attributes.positions.push(g.coordinates[j][i])
           draw.linestrip.count++
           if (f) {
-            var xattrs = f(m, g.coordinates[i], j, i, 1)
+            var xattrs = f(m, g.coordinates[j][i], j, i, 1)
             if (xattrs) {
               var keys = Object.keys(xattrs)
               for (var i = 0; i < keys.length; i++) {
@@ -175,7 +175,51 @@ module.exports = function (mesh, f) {
         }
       }
     } else if (g.type === 'Polygon') {
+      var data = earcut.flatten(g.coordinates)
+      var cells = earcut(data.vertices, data.holes, data.dimensions)
+      var len = draw.triangles.attributes.positions.length
+      for (var i = 0; i < cells.length; i++) {
+        draw.triangles.cells.push(len + cells[i])
+      }
+      for (var i = 0; i < data.vertices.length; i++) {
+        draw.triangles.attributes.positions.push(data.vertices[i])
+        if (f) {
+          var xattrs = f(m, data.vertices[i], i)
+          if (xattrs) {
+            var keys = Object.keys(xattrs)
+            for (var j = 0; j < keys.length; j++) {
+              if (!draw.triangles.attributes[keys[j]]) {
+                draw.triangles.attributes[keys[j]] = []
+              }
+              draw.triangles.attributes[keys[j]].push(xattrs[keys[j]])
+            }
+          }
+        }
+      }
     } else if (g.type === 'MultiPolygon') {
+      for (var j = 0; j < g.coordinates.length; j++) {
+        var data = earcut.flatten(g.coordinates[j])
+        var cells = earcut(data.vertices, data.holes, data.dimensions)
+        var len = draw.triangles.attributes.positions.length
+        for (var i = 0; i < cells.length; i++) {
+          draw.triangles.cells.push(len + cells[i])
+        }
+        for (var i = 0; i < data.vertices.length; i++) {
+          draw.triangles.attributes.positions.push(data.vertices[i])
+          if (f) {
+            var xattrs = f(m, data.vertices[i], j, i)
+            if (xattrs) {
+              var keys = Object.keys(xattrs)
+              for (var k = 0; k < keys.length; k++) {
+                if (!draw.triangles.attributes[keys[k]]) {
+                  draw.triangles.attributes[keys[k]] = []
+                }
+                draw.triangles.attributes[keys[k]].push(xattrs[keys[k]])
+              }
+            }
+          }
+        }
+      }
     } else if (g.type === 'GeometryCollection') {
     }
   }
